735a39ae552d87b73009e599484cc1f561387c3d
Daniel Magro <dando385@yahoo.com>
2024-05-09 12:58:24 -0400

    updating mongodb uri

--- utils/mongodb.js
+++ utils/mongodb.js
@@ -1,26 +1,24 @@
+// utils/mongodb.js
 import { MongoClient } from 'mongodb';
 
 const uri = process.env.MONGODB_URI;
-const options = {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-};
 
-let client;
-let clientPromise;
-
-if (!process.env.MONGODB_URI) {
+if (!uri) {
   throw new Error('Please add your Mongo URI to .env.local');
 }
 
+let client = new MongoClient(uri);
+let clientPromise;
+
+// In development mode, use a global variable so the value
+// is preserved across module reloads caused by HMR (Hot Module Replacement).
 if (process.env.NODE_ENV === 'development') {
   if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
-  client = new MongoClient(uri, options);
+  // In production mode, it's best to not use a global variable.
   clientPromise = client.connect();
 }
 
